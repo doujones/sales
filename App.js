44ed1a3b12aee3a68f074cae766257baccd127b7
Admin <admin@Williams-MacBook-Air.local>
2020-07-05 10:01:10 -0400

    second commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Bookables from './components/Bookables'
+
+function App() {
+  return (
+    <div className="App">
+      <Bookables/>
+    </div>
+  );
+}
+
+export default App;
